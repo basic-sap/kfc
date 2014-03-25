6baa3377b6a3b70283c3f5cf2242411a01085c3c
basic-sap <bizet.sap@outlook.com>
2014-03-25 19:15:36 +0800

    finance page: fix bs issue

--- js/finance.main.js
+++ js/finance.main.js
@@ -1,6 +1,6 @@
 
 require(['requirejs.config'] , function() {
-  require(['jquery', 'flot', 'flot_time', 'flot_valuelabels', 'flot_pie'
+  require(['jquery', 'bootstrap', 'flot', 'flot_time', 'flot_valuelabels', 'flot_pie'
    ], function($, _) {
     $(function(){
       require(['view/daily_fi.chart.view', 'view/item_profit.chart.view'], function(__Daily_Fi_Chart, __Item_Profit_Chart) {
