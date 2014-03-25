51edd1145aca3b94a3d64519d52c0aa0bf5ec65d
wsj <wsj@lab186lte248.(none)>
2014-03-25 14:25:19 +0800

    add chart

--- js/requirejs.config.js
+++ js/requirejs.config.js
@@ -7,10 +7,12 @@ define(function() {
       'jquery.cookie': 'lib/jquery.cookie',
       'bootstrap': 'lib/bootstrap-3.0.0/dist/js/bootstrap.min',
       'fuelux': 'lib/fuelux-master/dist',
-      'moment': 'lib/moment'
+      'moment': 'lib/moment',
+      'flot': 'lib/flot'
     },
     shim: {
-      'bootstrap':  ['jquery']
+      'flot/jquery.flot.min': {deps: ['jquery']},
+      'flot/jquery.flot.time.min': {deps: ['jquery', 'flot/jquery.flot.min']},
     }
   });})();
 });
